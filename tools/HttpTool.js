a518113f344a3114801ae751d3a4f0aed07cc241
manishbhaiii <manishkumar231007@gmail.com>
2026-08-29 13:27:36 +0530

    feat: expand allowed HTTP domains and enforce waifu.im sfw strictness

--- tools/HttpTool.js
+++ tools/HttpTool.js
@@ -1,21 +1,28 @@
 const ALLOWED_DOMAINS = [
-    "animechan.xyz",
-    "api.adviceslip.com",
-    "api.agify.io",
-    "api.coindesk.com",
-    "api.dictionaryapi.dev",
-    "api.genderize.io",
-    "api.github.com",
-    "api.jikan.moe",
-    "api.nationalize.io",
-    "catfact.ninja",
-    "dog.ceo",
-    "meowfacts.herokuapp.com",
-    "nekos.best",
-    "official-joke-api.appspot.com",
-    "pokeapi.co",
-    "randomfox.ca",
-    "v2.jokeapi.dev"
+    "amiiboapi.com", "anapioficeandfire.com", "animechan.xyz", "api.adviceslip.com", "api.agify.io",
+    "api.artic.edu", "api.aviationapi.com", "api.aviationstack.com", "api.bible-api.com", "api.carbonintensity.org.uk",
+    "api.cdnjs.com", "api.chess.com", "api.chucknorris.io", "api.citybik.es", "api.coindesk.com",
+    "api.coinpaprika.com", "api.country.is", "api.covid19api.com", "api.deezer.com", "api.dicebear.com",
+    "api.dictionaryapi.dev", "api.doge-meme.lol", "api.exchangerate-api.com", "api.exchangerate.host", "api.forismatic.com",
+    "api.frankfurter.app", "api.funtranslations.com", "api.gbif.org", "api.genderize.io", "api.generadordni.es",
+    "api.giphy.com", "api.github.com", "api.hackertarget.com", "api.harvardartmuseums.org", "api.hypixel.net",
+    "api.imgbb.com", "api.ipify.org", "api.jikan.moe", "api.kanye.rest", "api.le-systeme-solaire.net",
+    "api.lyrics.ovh", "api.mangadex.org", "api.mathjs.org", "api.mcsrvstat.us", "api.met.no",
+    "api.namefake.com", "api.nasa.gov", "api.nationalize.io", "api.nbp.pl", "api.open-meteo.com",
+    "api.openweathermap.org", "api.publicapis.org", "api.qrserver.com", "api.quotable.io", "api.radio-browser.info",
+    "api.rawg.io", "api.spacexdata.com", "api.spoonacular.com", "api.taylor.rest", "api.thecatapi.com",
+    "api.thedogapi.com", "api.thronesapi.com", "api.waifu.im", "api.weatherapi.com", "api.zippopotam.us",
+    "axoltlapi.herokuapp.com", "boringapi.com", "catfact.ninja", "collectionapi.metmuseum.org", "deckofcardsapi.com",
+    "disease.sh", "dog.ceo", "evilinsult.com", "favqs.com", "gutendex.com",
+    "http.cat", "http.dog", "httpbin.org", "icanhazdadjoke.com", "ipapi.co",
+    "ipinfo.io", "itunes.apple.com", "jsonplaceholder.typicode.com", "kitsu.io", "meowfacts.herokuapp.com",
+    "musicbrainz.org", "nekos.best", "nominatim.openstreetmap.org", "numbersapi.com", "official-joke-api.appspot.com",
+    "open.er-api.com", "openlibrary.org", "opentdb.com", "place.dog", "placekitten.com",
+    "pokeapi.co", "postman-echo.com", "random-d.uk", "randomfox.ca", "randomuser.me",
+    "reqres.in", "restcountries.com", "robohash.org", "shibe.online", "tenor.googleapis.com",
+    "trace.moe", "ui-avatars.com", "uinames.com", "uselessfacts.jsph.pl", "uuidgenerator.net",
+    "v2.jokeapi.dev", "wttr.in", "www.dnd5eapi.co", "www.fruityvice.com", "www.thecocktaildb.com",
+    "www.themealdb.com", "yesno.wtf", "zenquotes.io"
 ];
 
 const BLOCKED_KEYWORDS = [
@@ -42,6 +49,12 @@ async function httpRequest(url, headers = {}) {
         
         if (!isAllowed) {
             return { error: `Domain not allowed for security reasons. Allowed domains: ${ALLOWED_DOMAINS.join(", ")}` };
+        }
+        
+        if (hostname === "api.waifu.im") {
+            if (parsedUrl.searchParams.get('is_nsfw') !== 'false') {
+                return { error: "Security restriction: api.waifu.im must be called with ?is_nsfw=false" };
+            }
         }
         
         const urlString = parsedUrl.toString().toLowerCase();
